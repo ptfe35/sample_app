bcfd94b320fc74475eec3016e597dae1f00fc48b
ptfe35 <j3de35pys@gmail.com>
2020-08-24 13:46:13 +0900

    [Start] 3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -7,6 +7,12 @@ class TodolistsController < ApplicationController
     list.save
     redirect_to '/top'
   end
+  def index
+    @lists = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+  end
   private
   def list_params
     params.require(:list).permit(:title, :body)
