6ebe248546f832355a722e0c5761428ad535f283
ptfe35 <j3de35pys@gmail.com>
2020-08-24 16:06:59 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a1e4b12a283d80d69b883877f9bd052e2afe06dbc6c1529e8a91e84f2dec6a9c2f53b7cea4cf8ad3ee9cf30f53b6967e2d5eba0c6eb1d71461bc0329af7d97b3'
